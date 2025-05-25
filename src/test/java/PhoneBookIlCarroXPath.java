5cf2e1e2ab7a1ad092231a22729ebb22773e9a2e
ElenaMikhaleva <elena.mikhaleva@gmail.com>
2025-05-25 21:05:14 +0300

    corrected PhoneBookIlCarroXPath

--- src/test/java/PhoneBookIlCarroXPath.java
+++ src/test/java/PhoneBookIlCarroXPath.java
@@ -6,34 +6,35 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class PhonebookIlCarroXPath {
+public class PhoneBookIlCarroXPath {
+
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void xpathTest() {
+    public void xpathTest(){
         driver.get("https://telranedu.web.app/home");
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement div = driver.findElement(By.xpath("//div[@id='root']"));
+        WebElement div = driver.findElement(By.xpath("//div[@id='root']"));//CSS div[id='root']
         System.out.println(div.getText());
         WebElement btnAbout = driver.findElement(By.xpath("//a[text()='ABOUT']"));
         String str = btnAbout.getAttribute("href");
         System.out.println(str);
-        System.out.println("=====================");
-
+        System.out.println("=======================");
         WebElement btnLogin = driver.findElement(By.xpath("//a[@href='/login']"));
         btnLogin.click();
-        WebElement inputEmail = driver.findElement(By.xpath("input[@placeholder='Email']"));
-        inputEmail.sendKeys("zxc_mail23123@mymail.ru");
+        WebElement inputEmail = driver.findElement(By.xpath("//input[@placeholder='Email']"));
+        inputEmail.sendKeys("zxc_mail123@mymail.com");
         WebElement inputPassword = driver.findElement(By.xpath("//*[@name='password']"));
-        inputPassword.sendKeys("Password1234$");
+        inputPassword.sendKeys("Password123!");
         WebElement btnRegistration = driver.findElement(By.xpath("//*[text()='Registration']"));
         btnRegistration.click();
+        driver.quit();
     }
 
     @Test
-    public void ilCarroXPath() {
+    public void iLCarroXpath(){
         driver.get("https://ilcarro.web.app/search");
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
@@ -42,10 +43,16 @@ public class PhonebookIlCarroXPath {
         btnSignUp.click();
         WebElement inputName = driver.findElement(By.xpath("//input[@id='name']"));
         inputName.sendKeys("Bilbo");
-        WebElement inputLastName = driver.findElement(By.xpath("//input[@formcontrolname='lastName']"));
+        WebElement inputLastName =
+                driver.findElement(By.xpath("//input[@formcontrolname='lastName']"));
         inputLastName.sendKeys("Baggins");
         WebElement inputEmail = driver.findElement(By.xpath("//form/div[3]/input"));
-        inputEmail.sendKeys("bilbo_baggins_1234@mail.com");
-        ...
+        inputEmail.sendKeys("bilbo_baggins_12345@mail.com");
+        WebElement inputPassword = driver.findElement(By.xpath("//form/div[4]/input"));
+        inputPassword.sendKeys("Password123!");
+        WebElement checkBox = driver.findElement(By.xpath("//label[@for='terms-of-use']"));
+        checkBox.click();
+        WebElement btnYalla = driver.findElement(By.xpath("//button[@type='submit']"));
+        btnYalla.click();
     }
 }
